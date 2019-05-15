d6db76a2a83f3f3c29a0aaccdac12cf525eb5ad4
industrious-format-3756 <xavier.ruiz@yale.edu>
2019-05-15 02:51:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+katz_deli = []
+def line(name)
+  if katz_deli == []
+    "The line is currently empty."
+  else
+    place = katz_deli.index(name)
+    place + 1
+  end
+end
+
+def take_a_number(line, new)
